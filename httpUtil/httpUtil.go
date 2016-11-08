ff54950ffe9fe557edacd8d873a218d1a2cf516b
kevin-zx <519754821@qq.com>
2016-11-08 17:11:41 +0800

    add httpUtil

---
+++ httpUtil/httpUtil.go
@@ -0,0 +1,84 @@
+package httpUtil
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+//GetWebConFromUrl simply get web content
+//from net
+func GetWebConFromUrl(url string) (string, error) {
+	response, err := DoRequest(url, nil, "GET", nil, 10)
+	if err != nil {
+		return "", err
+	}
+	return getContentFromResponse(response)
+}
+
+//GetWebConFromUrlWithAllArgs get web content
+//with some args
+func GetWebConFromUrlWithAllArgs(url string, headerMap map[string]string, method string, postData []byte, timeOut time.Duration) (string, error) {
+	response, err := DoRequest(url, headerMap, method, postData, timeOut)
+	if err != nil {
+		return "", err
+	}
+	return getContentFromResponse(response)
+}
+
+//GetWebConFromUrlWithHeader get web con from target url
+//param headerMap is some header info
+func GetWebConFromUrlWithHeader(url string, headerMap map[string]string) (string, error) {
+	response, err := DoRequest(url, headerMap, "GET", nil, 10)
+	if err != nil {
+		return "", err
+	}
+	return getContentFromResponse(response)
+}
+
+func getContentFromResponse(response *http.Response) (string, error) {
+	defer response.Body.Close()
+	var c []byte
+	for {
+		buf := make([]byte, 1024)
+		n, err := response.Body.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err != nil && err != io.EOF {
+			return "", err
+		}
+
+		c = append(c, buf...)
+	}
+	return string(c), nil
+}
+
+func DoRequest(url string, headerMap map[string]string, method string, postData []byte, timeOut time.Duration) (*http.Response, error) {
+	timeout := time.Duration(timeOut * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	method = strings.ToUpper(method)
+	var req *http.Request
+	var err error
+	if postData != nil && method == "POST" {
+		req, err = http.NewRequest(method, url, bytes.NewReader(postData))
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		req, err = http.NewRequest(method, url, nil)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+	for key, value := range headerMap {
+		req.Header.Add(key, value)
+	}
+
+	return client.Do(req)
+}
---
+++ httpUtil/httpUtil_test.go
@@ -0,0 +1,67 @@
+package httpUtil
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+)
+
+func testGetWebConFromUrl(t testing.T) {
+	content, err := GetWebConFromUrl("https://www.baidu.com")
+	if err != nil {
+		t.Error(err)
+	}
+	if len(content) == 0 {
+		t.Error("get empty web content")
+	}
+
+}
+
+func testGetWebConFromUrlWithHeader(t testing.T) {
+	header := make(map[string]string)
+	header["Accept-Encoding"] = "gzip, deflate, sdch"
+	header["Accept-Language"] = "en-US,en;q=0.8"
+	header["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"
+	header["Accept"] = "*/*"
+	header["Referer"] = "http://www.jd.com/"
+	header["Connection"] = "keep-alive"
+	url := "http://dd-search.jd.com/?ver=2&zip=1&key=s&pvid=4a6j59vi.tnm8dd&t=1478588720776&curr_url=www.jd.com%2F&callback=jQuery1880422"
+	content, err := GetWebConFromUrlWithHeader(url, header)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(content) == 0 {
+		t.Error("get empty web content")
+	}
+}
+
+func testGetWebConFromUrlWithAllArgs(t testing.T) {
+	header := make(map[string]string)
+	header["Accept-Encoding"] = "gzip, deflate, sdch"
+	header["Accept-Language"] = "en-US,en;q=0.8"
+	header["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"
+	header["Accept"] = "*/*"
+	header["Referer"] = "http://www.jd.com/"
+	header["Connection"] = "keep-alive"
+	url := "http://dd-search.jd.com/?ver=2&zip=1&key=s&pvid=4a6j59vi.tnm8dd&t=1478588720776&curr_url=www.jd.com%2F&callback=jQuery1880422"
+	method := "get"
+	var pastStruct = []struct {
+		a string
+		b string
+	}{
+		{"2", "3"},
+		{"3", "4"},
+	}
+	postData, err := json.Marshal(pastStruct)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	timeOut := time.Duration(10)
+	content, err := GetWebConFromUrlWithAllArgs(url, header, method, postData, timeOut)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if len(content) == 0 {
+		t.Error("get empty web content")
+	}
+}
