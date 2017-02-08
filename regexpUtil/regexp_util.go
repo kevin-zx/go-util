b466ea715a47c328eab20977be417d6bea12560a
kevin-zx <519754821@qq.com>
2017-02-08 13:28:05 +0800

    增加regexp split

--- regexpUtil/regexp_util.go
+++ regexpUtil/regexp_util.go
@@ -10,3 +10,12 @@ func FindString(pattern string, line string) []string {
 	resultStr := re.FindAllString(line, 1)
 	return resultStr
 }
+
+func SplitString(s string, pattern string) []string {
+	re, _ := regexp.Compile(pattern)
+	return re.Split(s, -1)
+}
+
+func SplitString2Line(s string) []string {
+	return SplitString(s, "\r\n|\r|\n")
+}
